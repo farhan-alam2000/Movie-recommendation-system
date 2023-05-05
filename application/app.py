c52762637b7ede5c3f06385ee82d79fcc19ddf05
Farhan <alaamm.farhan@gmail.com>
2023-05-06 00:36:13 +0530

    model retrain backend code added

--- application/app.py
+++ application/app.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+# from model.run_example import retrainModel
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify, make_response
 from flask_pymongo import PyMongo
 import bcrypt
 from dotenv import load_dotenv
 import os
-import movie_poster
+import json
+# import movie_poster
+# import movie_poster
+import requests
+# from model import run_example
+# import sys
+# sys.path.append('/Users/varunjain/Desktop/Jumpstart-BTP/')
+# from model.run_example import getTopK
+# export PYTHONPATH=$PYTHONPATH:/Users/varunjain/Desktop/Jumpstart-BTP/model
+# from gettopk import getTopK
 
 load_dotenv()
 
@@ -24,31 +34,31 @@ def index():
 
 @app.route("/home", methods=['GET', 'POST'])
 def home():
-    if 'logged_in' not in session:
+    if 'logged_in' not in session or session['logged_in'] == False:
         return redirect(url_for('login'))
     if request.method == 'GET':
-        # Fetch movie data and store it in a list
-
-        # top_K_list = [1,2,3] -> 'models output' 
-        # for k in top_K_list:
-        #     movie_name = DBcallById()
-        movie_list = ["Batman", "The Avengers", "Toy Story"] # to be replaced by csv file
-        # ratings = [null, 3, 4] # display ratings
-
+        print_session_detail()
+        userId = int(session['userId'])
+        # movie_ids=getTopK(userId);
+        movie_ids=[1,2,3,4]
+        print(movie_ids)
+        # result=retrainModel(userId,[2,4,8],[5,3,2])
+        # print(result);
+    
         movies_data = []
-        for movie_name in movie_list:
+        for movie_id in movie_ids:
+            movie_id_str=str(movie_id);
+            movie = mongo.db.csvs.find_one({'movieId':movie_id_str})
+            movieName = str(movie["title"])
+            movie_name_without_year = movieName[:-6]
             movie_data = {}
-            movie_data["title"] = movie_name
-            movie_data["poster_path"] = movie_poster.get_movie_poster_url(movie_name)
+            movie_data["title"] = movie_name_without_year
+            movie_data["poster_path"] = get_movie_poster_url(movie_name_without_year)
+            movie_data["movie_id"] = movie_id
             movies_data.append(movie_data)
 
         # Render the home.html template and pass in the movie data
         return render_template("home.html", movies_data=movies_data)
-
-@app.route('/logout', methods = ['POST'])
-def logout():
-    session.pop('logged_in', None)
-    return redirect(url_for('login'))
 
 
 
@@ -93,6 +103,9 @@ def login():
             if bcrypt.checkpw(password, user['password']):
                 session['logged_in'] = True
                 session['email'] = email
+                session['userId']=str(user["userId"])
+                print("Login")
+                print(session['userId']);
                 return redirect(url_for('home'))
             else:
                 error = 'Invalid password'
@@ -103,30 +116,91 @@ def login():
     else:
         return render_template('login.html')
 
-
-
-@app.route('/matrix', methods=['POST'])
-def create_matrix():
+@app.route('/vote', methods=['POST'])
+def vote():
     data = request.get_json()
-    rows = data['rows']
-    cols = data['cols']
-    matrix = data['data']
-    # document-based format
-    matrix_doc = {
-        'name': 'URM',
-        'matrix': matrix,
-        'rows': rows,
-        'cols': cols
-    }
-    mongo.db.matrices.insert_one(matrix_doc)
-    return jsonify({'message': 'Matrix created successfully!'})
+    movie_id = int(data['movie_id'])
+    vote_value = int(data['vote_value'])
+    # do something with the vote data, like updating a database
 
-@app.route('/matrix', methods=['GET'])
-def fetch_matrix():
-    matrix = mongo.db.matrices.find_one({'name':'URM'})
-    # print(matrix)
-    return 
+    # Get the votes dictionary from the cookie or create a new one if it doesn't exist
+    votes_cookie = request.cookies.get('votes')
+    if votes_cookie:
+        votes = json.loads(votes_cookie)
+    else:
+        votes = {}
+
+    # Update the votes dictionary with the new vote
+    votes[movie_id] = vote_value
+    # print(votes[movie_id])
+    # print(len(votes))
+    
+
+    # Save the updated votes dictionary to a cookie and return a response
+    response = make_response('OK')
+    response.set_cookie('votes', value=json.dumps(votes))
+    
+    print_session_detail()
+    return response
+
+
+@app.route('/logout', methods = ['POST'])
+def logout():
+    print("Logout")
+    print_session_detail()
+    session.pop('logged_in', None)
+    session.pop('userId', None)
+    session.pop('email',None)
+    # session.pop('votes',None)
+    response = make_response(redirect(url_for('login')))
+    response.delete_cookie('votes')
+    return response
+
+# @app.route('/matrix', methods=['POST'])
+# def create_matrix():
+#     data = request.get_json()
+#     rows = data['rows']
+#     cols = data['cols']
+#     matrix = data['data']
+#     # document-based format
+#     matrix_doc = {
+#         'name': 'URM',
+#         'matrix': matrix,
+#         'rows': rows,
+#         'cols': cols
+#     }
+#     mongo.db.matrices.insert_one(matrix_doc)
+#     return jsonify({'message': 'Matrix created successfully!'})
+
+# @app.route('/matrix', methods=['GET'])
+# def fetch_matrix():
+#     matrix = mongo.db.matrices.find_one({'name':'URM'})
+#     # print(matrix)
+#     return 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+base_url = "http://www.omdbapi.com/"
+api_key = os.getenv("OMDB_API_KEY")
+
+def get_movie_poster_url(movie_name):
+    response = requests.get(base_url, params={"apikey": api_key, "t": movie_name})
+    data = response.json()
+    poster_url = data["Poster"]
+    return poster_url
+
+
+def print_session_detail():
+    if('userId' in session): 
+        print(session['userId'])
+    if('email' in session): 
+        print(session['email'])
+    votes_cookie = request.cookies.get('votes')
+    if votes_cookie:
+        votes = json.loads(votes_cookie)
+        for movie_id, vote_value in votes.items():
+            print(f"Movie ID {movie_id}: Vote value {vote_value}")
 
